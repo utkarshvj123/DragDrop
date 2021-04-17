195627cf78232a76e14cf654f341a2c7245afbbb
utkarsh vijayvargiya <utkarsh.innoventes@embibe.com>
2021-04-17 18:22:30 +0530

    last working

--- src/components/DragDrop/index.js
+++ src/components/DragDrop/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import * as Icon from "react-bootstrap-icons";
 
-// a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
