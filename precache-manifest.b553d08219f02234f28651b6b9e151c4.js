d24f20374f137f621b3390e598081e53c2548350
utkarsh vijayvargiya <utkarsh.innoventes@embibe.com>
2021-04-17 15:11:43 +0530

    Updates

---
+++ precache-manifest.b553d08219f02234f28651b6b9e151c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413490044fde9ad3a546cef3f4515099",
+    "url": "/draganddrop/index.html"
+  },
+  {
+    "revision": "b5c4e5a7b05d525bbf09",
+    "url": "/draganddrop/static/css/2.c3e9737a.chunk.css"
+  },
+  {
+    "revision": "810feb1b8ec3ebd5de20",
+    "url": "/draganddrop/static/css/main.ba3a4715.chunk.css"
+  },
+  {
+    "revision": "b5c4e5a7b05d525bbf09",
+    "url": "/draganddrop/static/js/2.222b2445.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/draganddrop/static/js/2.222b2445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810feb1b8ec3ebd5de20",
+    "url": "/draganddrop/static/js/main.947bb60a.chunk.js"
+  },
+  {
+    "revision": "f0cd2896e8528a576e88",
+    "url": "/draganddrop/static/js/runtime-main.ec1aa9f2.js"
+  },
+  {
+    "revision": "59ef1da45f25e6ed92342bf68cbde109",
+    "url": "/draganddrop/static/media/bg-abstract-resized.59ef1da4.png"
+  }
+]);
